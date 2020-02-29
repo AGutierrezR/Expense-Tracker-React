0bca4342d68c613a75c7f77d1203a61f9fb86d9c
Andres Gutierrez <agutierrez.er@gmail.com>
2020-02-29 03:05:43 +0100

    Create a Global Provider Component
    This component will share the global state on the entire app

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,4 +1,5 @@
 import React, { createContext, useReducer } from 'react'
+import AppReducer from './AppReducer'
 
 // Initial state
 const initialState = {
@@ -13,3 +14,16 @@ const initialState = {
 // Create context
 export const GlobalContext = createContext(initialState)
 
+// Provider Component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState)
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions
+      }}>
+      {children}
+    </GlobalContext.Provider>
+  )
+}
